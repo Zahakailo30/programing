6d3874b82f0a2b11ea8b2722cf7c7cf0805fc820
sofia zahakailo <sofiya.zahakaylo@lnu.edu.ua>
2021-09-17 10:35:34 +0300

    changed program(task 1)

--- task_1.py
+++ task_1.py
@@ -1,29 +1,35 @@
-def func():
+def reverseNegativeNumbers(array):
     negative = []
     negative_count = 0
     negative_size = 0
-
-    n = int(input("Введіть розмір масиву: "))
-    array = []
+    n = len(array)
     for i in range(n):
-        a = int(input("Введіть елемент масиву: "))
-        array.append(a)
-        if a < 0:
-            negative.append(a)
+        if array[i] < 0:
+            negative.append(array[i])
             negative_size += 1
-
     for i in range(n):
         if array[i] >= 0:
             continue
         array[i] = negative[negative_size - negative_count - 1]
         negative_count += 1
-
     print(array)
+    return array
+
+
+def input_array():
+    n = int(input("Введіть розмір масиву: "))
+    array = []
+
+    for i in range(n):
+        a = int(input("Введіть елемент масиву: "))
+        array.append(a)
+    print(array)
+    return array
 
 try:
-    func()
+   array = input_array()
+   reverseNegativeNumbers(array)
 except ValueError:
     print("Використано не відповідний тип даних")
 except Exception:
     print("Помилка!")
-
