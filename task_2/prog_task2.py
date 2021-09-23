6b10b020ab67b8ff08791712ba85767056cbe3b9
sofia zahakailo <sofiya.zahakaylo@lnu.edu.ua>
2021-09-23 22:17:40 +0300

    Change program 7 variant

--- task_2/prog_task2.py
+++ task_2/prog_task2.py
@@ -1,54 +1,59 @@
 import random
-
-def move_max_el(n):
-    if(n % 2 == 0):
+def input_matrix():
+    n = int(input('Введіть порядок: '))
+    if (n % 2 == 0):
         print("Введіть непарне число!")
-    elif(n < 0):
+    elif (n < 0):
         print("Введіть додатнє число!")
     else:
         matrix = []
         for i in range(n):
             matr = []
             for j in range(n):
-                matr.append(random.randrange(0, 100))
+                matr.append(int(input("Введіть елемент:\n")))
             matrix.append(matr)
 
-        def print_m(matrix):
-            k = len(matrix)
-            for i in range(k):
-                for j in range(k):
-                    print(matrix[i][j], end=' ')
-                print()
-            print(' ')
-        print_m(matrix)
+        print_matrix(matrix)
+        return matrix
+def print_matrix(matrix):
+    k = len(matrix)
+    for i in range(k):
+        for j in range(k):
+            print(matrix[i][j], end=' ')
+        print()
+    print(' ')
+def move_max_el(matrix):
+    i1 = 0
+    j1 = 0
+    n = len(matrix)
+    max = matrix[0][0]
+    for i in range(n):
+        for j in range(n):
+            if (i == j or j == n - i - 1):
+                if (max < matrix[i][j]):
+                    max = matrix[i][j]
+                    i1 = i
+                    j1 = j
 
+    temp = matrix[n // 2][n // 2]
+    matrix[n // 2][n // 2] = matrix[i1][j1]
+    matrix[i1][j1] = temp
 
-        i1 = 0
-        j1 = 0
-        max = matrix[0][0]
-        for i in range(n):
-            for j in range(n):
-                if (i == j or j == n - i - 1):
-                    if (max < matrix[i][j]):
-                        max = matrix[i][j]
-                        i1 = i
-                        j1 = j
-
-        print("Найбільший елемент головної і бічної діагональ: ", max)
-
-        temp = matrix[n // 2][n // 2]
-        matrix[n // 2][n // 2] = matrix[i1][j1]
-        matrix[i1][j1] = temp
-
-        print_m(matrix)
-
-
+    print_matrix(matrix)
+    return matrix
 
 try:
-    n = int(input('Введіть порядок: '))
-    move_max_el(n)
+    print("Оберіть опцію:\n 1 - виконати завдання \n 2 - завершити роботу \n", end=" ")
+    P = int(input(""))
+    while (P == 1):
+        matrix1 = input_matrix()
+        move_max_el(matrix1)
+        print("Оберіть опцію:\n 1 - виконати завдання \n 2 - завершити роботу \n", end=" ")
+        P = int(input(""))
+    if (P == 2):
+        print("Роботу завершено")
+
 except ValueError:
     print("Неправильний тип даних!")
 except Exception:
     print("Помилка!")
-
